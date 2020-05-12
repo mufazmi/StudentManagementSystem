a4af9a25b3b0ac715f5bdf98e51d1229ada28a77
Social Codia <socialcodia@gmail.com>
2020-05-13 03:35:48 +0530

    Started work with rest api, added Api Interface and dot dot dot...

---
+++ app/src/main/java/com/socialcodia/studentmanagementsystem/Api.java
@@ -0,0 +1,16 @@
+package com.socialcodia.studentmanagementsystem;
+
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.POST;
+
+public interface Api {
+
+    @POST("register")
+    Call<ResponseBody> register(
+            @Field("name") String name,
+            @Field("email") String email,
+            @Field("password") String password
+    );
+}
